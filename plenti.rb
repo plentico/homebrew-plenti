89d9eef50f44473e29b2e25a17c46c871f7dcbb8
jimafisk <jimafisk@gmail.com>
2020-05-26 20:50:08 -0400

    Initial commit.

---
+++ plenti.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Plenti < Formula
+  desc "Static Site Generator with Go backend and Svelte frontend"
+  homepage ""
+  url "https://github.com/plentico/plenti/releases/download/v0.1.10/plenti_0.1.10_Mac_64-bit.tar.gz"
+  sha256 "cd85182a41da5810149b021b46b3d96d9f1ec295bc9196b326066540112b49d7"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test plenti`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
