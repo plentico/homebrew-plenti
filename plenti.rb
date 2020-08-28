e35c19f812c2238011bdc8c4c52c8fe3ed08af95
goreleaserbot <goreleaser@carlosbecker.com>
2020-08-27 20:17:40 -0400

    Brew formula update for plenti version v0.2.36

--- plenti.rb
+++ plenti.rb
@@ -2,16 +2,16 @@
 class Plenti < Formula
   desc "Dead simple SSG with Svelte frontend and Go CLI."
   homepage "https://plenti.co/"
-  version "0.2.35"
+  version "0.2.36"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/plentico/plenti/releases/download/v0.2.35/plenti_0.2.35_Mac_64-bit.tar.gz"
-    sha256 "545034b178c9b1291a5abcb7bf511c7c8825ea8a31c3e7ce4d2c75b3761b19e7"
+    url "https://github.com/plentico/plenti/releases/download/v0.2.36/plenti_0.2.36_Mac_64-bit.tar.gz"
+    sha256 "fa279c83f1cf1943c0b5e609f35b874935e97bec290ed8fb9781f661ca8fe1cc"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/plentico/plenti/releases/download/v0.2.35/plenti_0.2.35_Linux_64-bit.tar.gz"
-      sha256 "e51f55b81abaec38cf1accab28f128b81a24b2d511a4e3b23bc9db89eb1ad7d3"
+      url "https://github.com/plentico/plenti/releases/download/v0.2.36/plenti_0.2.36_Linux_64-bit.tar.gz"
+      sha256 "d4399b17bab97140a6f989e4caf1ea64f90b197e67d042b876bd79b66adbc870"
     end
   end
 
